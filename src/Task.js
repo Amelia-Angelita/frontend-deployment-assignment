57e07cab62d819fbe2b5213baaf3fe0ea1f83d3d
Amelia-Angelita <amelangeell@gmail.com>
2022-12-16 23:35:05 +0700

    deploy to netlify

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Amelia Angelita Silalahi" // TODO: replace this
+const studentId = "FE3192842" // TODO: replace this
+
+export { url, studentName, studentId }
